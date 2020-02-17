0d414cb20174790872100422009e711377f3079d
vinhali <43426743+vinhali@users.noreply.github.com>
2020-02-17 13:07:39 -0300

    Update structureDatabase.sql

--- postgres/structureDatabase.sql
+++ postgres/structureDatabase.sql
@@ -1,3 +1,25 @@
+-------------------------------------------------------------------------------------------------------------
+-- filename: structureDatabase.sql
+-- Introduction
+-- Script to be used in advanced monitoring
+--
+-------------------------------------------------------------------------------------------------------------
+-- Copyright
+--
+-- Copyright (C) 1989, 1991 Free Software Foundation, Inc., [http://fsf.org/]
+-- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+-- Everyone is permitted to copy and distribute verbatim copies
+-- of this license document, but changing it is not allowed.
+--
+-------------------------------------------------------------------------------------------------------------
+-- Version:      1.0.0
+-- Author:       Luis Henrique Vinhali <vinhali@outlook.com>
+--
+-- Changelog:
+-- 1.0.0 02-02-2020      Inital version
+--
+-------------------------------------------------------------------------------------------------------------
+
 CREATE DATABASE networkNeural;
 
 CREATE TABLE dataSet(
