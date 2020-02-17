ee80e5624c845d5a463d9830fdb459f511f3c7ff
vinhali <43426743+vinhali@users.noreply.github.com>
2020-02-17 13:05:04 -0300

    Update getValues.py

--- scripts/getValues.py
+++ scripts/getValues.py
@@ -1,3 +1,27 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+# filename: getValues.py
+#-----------------------------------------------------------------------------------------------------------
+# Introduction
+# Script to be used in advanced monitoring
+#
+#-----------------------------------------------------------------------------------------------------------
+# Copyright
+#
+# Copyright (C) 1989, 1991 Free Software Foundation, Inc., [http://fsf.org/]
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
+#
+#-----------------------------------------------------------------------------------------------------------
+# Version:      1.0.0
+# Author:       Luis Henrique Vinhali <vinhali@outlook.com>
+#
+# Changelog:
+# 1.0.0 02-02-2020      Inital version
+#
+#-----------------------------------------------------------------------------------------------------------
+
 from zabbix.api import ZabbixAPI
 import psycopg2
 from datetime import datetime, date, time, timedelta
