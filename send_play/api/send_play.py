6b96e8d912127c9136a387f63b1fb8298a8ca631
vinhali <43426743+vinhali@users.noreply.github.com>
2020-03-24 18:28:03 -0300

    Update send_play.py

--- send_play/api/send_play.py
+++ send_play/api/send_play.py
@@ -2,19 +2,107 @@
 import os
 import sys
 import json
+import psycopg2
+from random import randrange
 from ansible import context
 from ansible.cli import CLI
 from ansible.module_utils.common.collections import ImmutableDict
 from ansible.executor.playbook_executor import PlaybookExecutor
 from ansible.parsing.dataloader import DataLoader
 from ansible.inventory.manager import InventoryManager
+from ansible.utils.vars import load_extra_vars
+from ansible.utils.vars import load_options_vars
 from ansible.vars.manager import VariableManager
 from ansible.errors import AnsibleError, AnsibleParserError, AnsibleUndefinedVariable, AnsibleFileNotFound, AnsibleAssertionError, AnsibleTemplateError
 from ansible.plugins.callback import CallbackBase
 
+IDOPeration = randrange(10000000)
+hosts_path = '/etc/ansible/hosts'
+play_path = '/etc/ansible/playbooks/'
+
 class ModuleResultsCollector():
 
-    def run_playbook(self, affected_host, host_file, playbook_path, selectuser, selectpass, extra_vars=None):
+    def connpg(self, query):
+        # Connection with postgres
+        connpostgres = psycopg2.connect("host='192.168.1.138'"
+                                " dbname='networkneural'"
+                                " user='postgres'"
+                                " password='postgres'")
+        cursorpost = connpostgres.cursor()
+        cursorpost.execute(query)
+        connpostgres.commit()
+        cursorpost.close
+        connpostgres.close()
+
+    def header(self, user, frequency, playbook, customer, host):
+        print("******************************************************************")
+        print('''Starting with config:
+        User: {0}
+        Frequency: {1}
+        Playbook: {2}
+        Customer: {3}
+        Host: {4}'''.format(user,frequency,playbook,customer,host))
+        print("******************************************************************")
+        flow = ModuleResultsCollector()
+        flow.running(IDOPeration, user, frequency, playbook, customer, host)
+
+    def passed(self, IDOPeration, status):
+
+        print("Passed       *****************************************************")
+
+        try:
+            flow = ModuleResultsCollector()
+            flow.connpg("UPDATE \"ANSIBLE_HISTORY\" SET status = '{1}' WHERE idoPeration = '{0}'"\
+            .format(IDOPeration, status))
+
+        except:
+
+            print("Failed commit --> Status = PASSED ********************************")
+
+        finally:
+
+            print("End script   *****************************************************")
+            print("******************************************************************")
+
+    def failed(self, IDOPeration, status):
+
+        print("Failed start *****************************************************")
+
+        try:
+
+            flow = ModuleResultsCollector()
+            flow.connpg("UPDATE \"ANSIBLE_HISTORY\" SET status = '{1}' WHERE idoPeration = '{0}'"\
+            .format(IDOPeration, status))
+
+        except:
+
+            print("Failed commit --> Status = FAILED ********************************")
+
+        finally:
+
+            print("End script   *****************************************************")
+            print("******************************************************************")
+
+    def running(self, IDOPeration, user, frequency, playbook, customer, host):
+
+        print("Start play   *****************************************************")
+
+        try:
+
+            flow = ModuleResultsCollector()
+            flow.connpg('''INSERT INTO \"ANSIBLE_HISTORY\"
+            (idoPeration, playbook,technican,status,frequency,customer,host,startdate)
+            VALUES ('{0}', '{3}','{1}','RUNNING','{2}','{4}','{5}',current_timestamp)'''
+            .format(IDOPeration, user,frequency,playbook,customer,host))
+            print("Started      *****************************************************")
+            flow.run_playbook(host, hosts_path, play_path+playbook, user)
+
+        except:
+
+            flow = ModuleResultsCollector()
+            flow.failed(IDOPeration, 'FAILED')
+
+    def run_playbook(self, affected_host, host_file, playbook_path, selectuser):
 
         if not os.path.exists(playbook_path):
             print('[INFO] The playbook does not exist: "{0}"'.format(playbook_path))
@@ -28,18 +116,16 @@ class ModuleResultsCollector():
             try:
 
                 loader = DataLoader()
-                passwords = dict(vault_pass=selectpass)
+                passwords = dict(vault_pass='pass')
 
                 context.CLIARGS = ImmutableDict(tags={}, listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh',
-                                    module_path=None, forks=100, remote_user=selectuser, private_key_file=None,
+                                    module_path=None, forks=100, remote_user=selectuser, private_key_file=None, extra_vars=[{'affected_hosts':''+affected_host+''}],
                                     ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=True,
                                     become_method='sudo', become_user=selectuser, verbosity=True, check=False, start_at_task=None)
 
                 inventory = InventoryManager(loader=loader, sources=(host_file))
 
                 variable_manager = VariableManager(loader=loader, inventory=inventory, version_info=CLI.version_info(gitinfo=False))
-
-                #variable_manager.extra_vars = {'affected_hosts': 'webserver'}
 
                 pbex = PlaybookExecutor(playbooks=[playbook_path], inventory=inventory, variable_manager=variable_manager, loader=loader, passwords=passwords)
 
@@ -50,29 +136,35 @@ class ModuleResultsCollector():
 
                     if(check[affected_host] == 2):
     
+                        flow = ModuleResultsCollector()
+                        flow.failed(IDOPeration, 'FAILED')
                         print('[INFO] Playbook failed: {}'.format(playbook_path))
 
                     elif (check[affected_host] == 4):
                         
+                        flow = ModuleResultsCollector()
+                        flow.failed(IDOPeration, 'FAILED')
                         print('[INFO] Playbook unreachable: {}'.format(playbook_path))
 
                     else:
 
+                        flow = ModuleResultsCollector()
+                        flow.passed(IDOPeration, 'PASSED')
                         print('[INFO] Playbook pass: {} [ok]'.format(playbook_path))
 
                 except AnsibleError as ansError:
 
+                    flow = ModuleResultsCollector()
+                    flow.failed(IDOPeration, 'FAILED')
                     print('[INFO] Ansible error: {}'.format(ansError))
 
             except Exception as e:
 
+                flow = ModuleResultsCollector()
+                flow.failed(IDOPeration, 'FAILED')
                 print('[INFO]: {} - Failed caused by: {}'.format(playbook_path,e))
 
-selectuser = 'root'
-selectpass = 'root'
-selectplay = 'GEN_CLEAN_MEMORY_LINUX.yml'
-host_file = '/etc/ansible/hosts'
-affected_host = 'hostname-here'
-
-flow = ModuleResultsCollector()
-flow.run_playbook(affected_host, host_file, '/etc/ansible/playbooks/'+selectplay+'', selectuser, selectpass)
+if __name__ == "__main__":
+    
+    flow = ModuleResultsCollector()
+    flow.header(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
