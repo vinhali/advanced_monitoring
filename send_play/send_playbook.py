608d7600ad39be32968e914fc826f4baba64988d
vinhali <43426743+vinhali@users.noreply.github.com>
2020-03-24 19:06:29 -0300

    Update send_playbook.py

--- send_play/send_playbook.py
+++ send_play/send_playbook.py
@@ -1,110 +1,40 @@
 import sys
-import psycopg2
 from datetime import datetime
 import sys
 import paramiko
-from random import randrange
 
-IDOPeration = randrange(10000000)
+class ModuleResultsCollector():
 
-def connpg(query):
-    # Connection with postgres
-	connpostgres = psycopg2.connect("host='192.168.1.138'"
-							" dbname='networkneural'"
-							" user='postgres'"
-							" password='postgres'")
-	cursorpost = connpostgres.cursor()
-	cursorpost.execute(query)
-	connpostgres.commit()
-	cursorpost.close
-	connpostgres.close()
+    def header(self, user, frequency, playbook, customer, host):
+        print("******************************************************************")
+        print('''Send with config:
+        User: {0}
+        Frequency: {1}
+        Playbook: {2}
+        Customer: {3}
+        Host: {4}'''.format(user,frequency,playbook,customer,host))
+        print("******************************************************************")
+        flow = ModuleResultsCollector()
+        flow.startplaybook(user, frequency, playbook, customer, host)
 
-def header(user, frequency, playbook, customer, host, pwd):
-	print("******************************************************************")
-	print('''Starting with config:
-	User: {0}
-	Frequency: {1}
-	Playbook: {2}
-	Customer: {3}
-	Host: {4}'''.format(user,frequency,playbook,customer,host))
-	print("******************************************************************")
-	running(IDOPeration, user, frequency, playbook, customer, host, pwd)
+    def startplaybook(self, user, frequency, playbook, customer, host):
+            
+        try:
 
-def passed(IDOPeration, status):
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect('192.168.1.139', username='root', password='root') # use shared key - best pratcies
 
-	print("Passed       *****************************************************")
+            stdin, stdout, stderr = client.exec_command('python /etc/ansible/scripts/activePlay.py {0} {1} {2} {3} {4}'.format(user, frequency, playbook, customer, host), get_pty=True)
 
-	try:
+            for line in iter(lambda: stdout.readline(2048), ""): 
+                print(line, end="")
 
-		connpg("UPDATE \"ANSIBLE_HISTORY\" SET status = '{1}' WHERE idoPeration = '{0}'"\
-		.format(IDOPeration, status))
+        except Exception as e:
 
-	except:
-
-		print("Failed commit --> Status = PASSED ********************************")
-
-	finally:
-
-		print("End script   *****************************************************")
-		print("******************************************************************")
-
-def failed(IDOPeration, status):
-
-	print("Failed start *****************************************************")
-
-	try:
-
-		connpg("UPDATE \"ANSIBLE_HISTORY\" SET status = '{1}' WHERE idoPeration = '{0}'"\
-		.format(IDOPeration, status))
-
-	except:
-
-		print("Failed commit --> Status = FAILED ********************************")
-
-	finally:
-
-		print("End script   *****************************************************")
-		print("******************************************************************")
-
-def running(IDOPeration, user, frequency, playbook, customer, host, pwd):
-
-	print("Start play   *****************************************************")
-
-	try:
-
-		connpg('''INSERT INTO \"ANSIBLE_HISTORY\"
-		(idoPeration, playbook,technican,status,frequency,customer,host,startdate)
-		VALUES ('{0}', '{3}','{1}','RUNNING','{2}','{4}','{5}',current_timestamp)'''
-		.format(IDOPeration, user,frequency,playbook,customer,host))
-		print("Started      *****************************************************")
-
-		startplaybook(playbook,user,host, pwd)
-
-	except:
-
-		failed(IDOPeration, 'FAILED')
-
-def startplaybook(name,user,iventory, pwd):
-    	
-	try:
-
-		client = paramiko.SSHClient()
-		client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		client.connect('192.168.1.139', username='root', password='root')
-
-		stdin, stdout, stderr = client.exec_command('ansible-playbook /etc/ansible/playbooks/'+name+' -e "ansible_user='+user+' ansible_password='+pwd+'" --extra-vars "host='+iventory+'"', get_pty=True)
-
-		for line in iter(lambda: stdout.readline(2048), ""): 
-			print(line, end="")
-
-		client.close()
-
-		passed(IDOPeration, 'PASSED')
-
-	except:
-
-		failed(IDOPeration, 'FAILED')
+            print('Failed caused by: {}', e)
 
 if __name__ == "__main__":
-    	
-    header(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6])
+    
+    flow = ModuleResultsCollector()
+    flow.header(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
