fd21be571979ac439d5f70395545bca37bb6b99d
vinhali <43426743+vinhali@users.noreply.github.com>
2020-03-26 20:42:49 -0300

    Update activePlay.py

--- ansible/scripts/activePlay.py
+++ ansible/scripts/activePlay.py
@@ -46,7 +46,7 @@ class ModuleResultsCollector():
         flow = ModuleResultsCollector()
         flow.running(IDOPeration, user, frequency, playbook, customer, host)
 
-    def passed(self, IDOPeration, status):
+    def passed(self, IDOPeration, affected_host, namePlay, status):
 
         print("Passed       *****************************************************")
 
@@ -54,6 +54,18 @@ class ModuleResultsCollector():
             flow = ModuleResultsCollector()
             flow.connpg("UPDATE \"ANSIBLE_HISTORY\" SET status = '{1}' WHERE idoPeration = '{0}'"\
             .format(IDOPeration, status))
+
+            try:
+
+                os.system('''php /etc/ansible/open_tickets/open.php eventhost="{}" event="OPEN" state="PASSED" hostproblemid=0 lasthostproblemid=0 servico="{}" triggerid="{}" eventansible="Dynamic - AUTO RESOLVED"'''
+                .format(affected_host,namePlay,IDOPeration))
+
+                print("Created ticket [ok]")
+                
+
+            except Exception as e:
+
+                print("[INFO] Failed open ticket - Caused by: {}".format(e))
 
         except:
 
@@ -64,7 +76,7 @@ class ModuleResultsCollector():
             print("End script   *****************************************************")
             print("******************************************************************")
 
-    def failed(self, IDOPeration, status):
+    def failed(self, IDOPeration, affected_host, namePlay, status):
 
         print("Failed start *****************************************************")
 
@@ -77,6 +89,18 @@ class ModuleResultsCollector():
         except:
 
             print("Failed commit --> Status = FAILED ********************************")
+
+        try:
+
+            os.system('''php /etc/ansible/open_tickets/open.php eventhost="{}" event="OPEN" state="FAILED" hostproblemid=0 lasthostproblemid=0 servico="{}" triggerid="{}" eventansible="Dynamic - AUTO RESOLVED"'''
+            .format(affected_host,namePlay,IDOPeration))
+            
+            print("Created ticket for failed in playbook[ok]")
+            
+
+        except Exception as e:
+
+            print("[INFO] Failed open ticket - Caused by: {}".format(e))
 
         finally:
 
@@ -94,75 +118,76 @@ class ModuleResultsCollector():
             (idoPeration, playbook,technican,status,frequency,customer,host,startdate)
             VALUES ('{0}', '{3}','{1}','RUNNING','{2}','{4}','{5}',current_timestamp)'''
             .format(IDOPeration, user,frequency,playbook,customer,host))
+
             print("Started      *****************************************************")
-            flow.run_playbook(host, hosts_path, play_path+playbook, user)
+
+            flow.run_playbook(host, hosts_path, play_path+playbook, playbook, user)
 
         except:
 
             flow = ModuleResultsCollector()
-            flow.failed(IDOPeration, 'FAILED')
+            flow.failed(IDOPeration, host,playbook, 'FAILED')
 
-    def run_playbook(self, affected_host, host_file, playbook_path, selectuser):
+    def run_playbook(self, affected_host, host_file, playbook_path, namePlay, selectuser):
 
         if not os.path.exists(playbook_path):
             print('[INFO] The playbook does not exist: "{0}"'.format(playbook_path))
             sys.exit()
 
-        elif not os.path.isfile(host_file):
+        if not os.path.isfile(host_file):
             print('[INFO] Host file does not exist: "{0}"'.format(host_file))
+            sys.exit()
 
-        else:
+        try:
+
+            loader = DataLoader()
+            passwords = dict(vault_pass='pass')
+
+            context.CLIARGS = ImmutableDict(tags={}, listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh',
+                                module_path=None, forks=100, remote_user=selectuser, private_key_file=None, extra_vars=[{'affected_hosts':''+affected_host+''}],
+                                ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=True,
+                                become_method='sudo', become_user=selectuser, verbosity=True, check=False, start_at_task=None)
+
+            inventory = InventoryManager(loader=loader, sources=(host_file))
+
+            variable_manager = VariableManager(loader=loader, inventory=inventory, version_info=CLI.version_info(gitinfo=False))
+
+            pbex = PlaybookExecutor(playbooks=[playbook_path], inventory=inventory, variable_manager=variable_manager, loader=loader, passwords=passwords)
 
             try:
 
-                loader = DataLoader()
-                passwords = dict(vault_pass='pass')
+                execPlay = json.dumps({affected_host: pbex.run()}, indent=4)
+                check = json.loads(execPlay)
 
-                context.CLIARGS = ImmutableDict(tags={}, listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh',
-                                    module_path=None, forks=100, remote_user=selectuser, private_key_file=None, extra_vars=[{'affected_hosts':''+affected_host+''}],
-                                    ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=True,
-                                    become_method='sudo', become_user=selectuser, verbosity=True, check=False, start_at_task=None)
-
-                inventory = InventoryManager(loader=loader, sources=(host_file))
-
-                variable_manager = VariableManager(loader=loader, inventory=inventory, version_info=CLI.version_info(gitinfo=False))
-
-                pbex = PlaybookExecutor(playbooks=[playbook_path], inventory=inventory, variable_manager=variable_manager, loader=loader, passwords=passwords)
-
-                try:
-
-                    execPlay = json.dumps({affected_host: pbex.run()}, indent=4)
-                    check = json.loads(execPlay)
-
-                    if(check[affected_host] == 2):
-    
-                        flow = ModuleResultsCollector()
-                        flow.failed(IDOPeration, 'FAILED')
-                        print('[INFO] Playbook failed: {}'.format(playbook_path))
-
-                    elif (check[affected_host] == 4):
-                        
-                        flow = ModuleResultsCollector()
-                        flow.failed(IDOPeration, 'FAILED')
-                        print('[INFO] Playbook unreachable: {}'.format(playbook_path))
-
-                    else:
-
-                        flow = ModuleResultsCollector()
-                        flow.passed(IDOPeration, 'PASSED')
-                        print('[INFO] Playbook pass: {} [ok]'.format(playbook_path))
-
-                except AnsibleError as ansError:
+                if(check[affected_host] == 2):
 
                     flow = ModuleResultsCollector()
-                    flow.failed(IDOPeration, 'FAILED')
-                    print('[INFO] Ansible error: {}'.format(ansError))
+                    flow.failed(IDOPeration, affected_host, namePlay, 'FAILED')
+                    print('[INFO] Playbook failed: {}'.format(playbook_path))
 
-            except Exception as e:
+                elif (check[affected_host] == 4):
+                    
+                    flow = ModuleResultsCollector()
+                    flow.failed(IDOPeration, affected_host, namePlay, 'FAILED')
+                    print('[INFO] Playbook unreachable: {}'.format(playbook_path))
+
+                else:
+
+                    flow = ModuleResultsCollector()
+                    flow.passed(IDOPeration, affected_host, namePlay, 'PASSED')
+                    print('[INFO] Playbook pass: {} [ok]'.format(playbook_path))
+
+            except AnsibleError as ansError:
 
                 flow = ModuleResultsCollector()
-                flow.failed(IDOPeration, 'FAILED')
-                print('[INFO]: {} - Failed caused by: {}'.format(playbook_path,e))
+                flow.failed(IDOPeration, affected_host, namePlay, 'FAILED')
+                print('[INFO] Ansible error: {}'.format(ansError))
+
+        except Exception as e:
+
+            flow = ModuleResultsCollector()
+            flow.failed(IDOPeration, affected_host, namePlay, 'FAILED')
+            print('[INFO]: {} - Failed caused by: {}'.format(playbook_path,e))
 
 if __name__ == "__main__":
     
