9b40323b1e43a0bee958b21dbf29d971d61a2c7a
vinhali <43426743+vinhali@users.noreply.github.com>
2020-03-28 17:37:43 -0300

    Update getValues.py

--- scripts/getValues.py
+++ scripts/getValues.py
@@ -166,7 +166,7 @@ if __name__ == "__main__":
     #flow.loginAPI(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],
     #sys.argv[5],sys.argv[6],sys.argv[7],sys.argv[8],sys.argv[9],sys.argv[10]) # Send external command python
 
-# login API respective values:
+# API respective values:
 # Value1 1 --> URL ZABBIX
 # Value1 2 --> USER ZABBIX
 # Value1 3 --> PASSWORD ZABBIX
