5586af87eecdfd301eaa6d73b09593a7ccb95757
vinhali <43426743+vinhali@users.noreply.github.com>
2020-03-30 21:19:00 -0300

    Update flask-api.py

--- flask/flask-api.py
+++ flask/flask-api.py
@@ -5,16 +5,36 @@ from bson import json_util
 import json
 import psycopg2
 from flask import Flask
+from flask import current_app
+from flask_httpauth import HTTPBasicAuth
 
 db_connect = create_engine('postgresql+psycopg2://postgres:postgres@localhost/networkneural')
+conn = db_connect.connect()
 app = Flask(__name__)
 api = Api(app)
+auth = HTTPBasicAuth()
+
+class Credentials(Resource):
+    @auth.verify_password
+    def verify_password(username, password):
+        try:
+            query = conn.execute('''select username, password from \"USERS_ANSIBLE\" 
+            WHERE username = '{}' and password = '{}'; '''.format(username, password))
+            result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+            resultDumps =  json.dumps(result[0], default=json_util.default)
+            indentify =  json.loads(resultDumps)
+            if indentify['username'] == username and indentify['password'] == password:
+                return True
+            else:
+                return False
+        except:
+            return False
 
 class ApiZabbix(Resource):
     @app.route("/getMemory")
+    @auth.login_required
     def getMemory():
         try:
-            conn = db_connect.connect()
             query = conn.execute("select * from \"MEMORYEXPORTZB\"")
             result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
             return json.dumps(result, default=json_util.default)
@@ -22,9 +42,9 @@ class ApiZabbix(Resource):
             print("[ALERT] Error caused by: {}".format(e))
 
     @app.route("/getCpu")
+    @auth.login_required
     def getCpu():
         try:
-            conn = db_connect.connect()
             query = conn.execute("select * from \"CPUEXPORTZB\"")
             result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
             return json.dumps(result, default=json_util.default)
@@ -32,9 +52,9 @@ class ApiZabbix(Resource):
             print("[ALERT] Error caused by: {}".format(e))
 
     @app.route("/getDisk")
+    @auth.login_required
     def getDisk():
         try:
-            conn = db_connect.connect()
             query = conn.execute("select * from \"DISKEXPORTZB\"")
             result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
             return json.dumps(result, default=json_util.default)
