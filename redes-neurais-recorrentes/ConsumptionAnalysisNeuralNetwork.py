630954bacc97962159ed5a722cff826046b114ed
vinhali <43426743+vinhali@users.noreply.github.com>
2020-04-02 20:15:34 -0300

    Update ConsumptionAnalysisNeuralNetwork.py

--- redes-neurais-recorrentes/ConsumptionAnalysisNeuralNetwork.py
+++ redes-neurais-recorrentes/ConsumptionAnalysisNeuralNetwork.py
@@ -33,11 +33,19 @@ import numpy as np
 from sklearn.metrics import mean_absolute_error
 from datetime import datetime
 
+class initTraining():
+
+    def startProcessing(self):
+
+        init = neuralAnalisys()
+        init.getValuesMemory()
+
 class neuralAnalisys():
 
-    def getValuesAPI(self):
+    def getValuesMemory(self):
 
         dataSet = []
+        start = neuralAnalisys()
 
         try:
 
@@ -53,104 +61,106 @@ class neuralAnalisys():
             for line in response_data:
                 dataSet.append(np.array([line['datecollect']['$date'],line['hostname'],line['historyvalue']]))
 
+            return start.neuralTraining(dataSet)
+
         except Exception as e:
             print("[ERROR] Error caused by: {}".format(e))
 
-        return dataSet
+    def neuralTraining(self, dataSet):
 
-    def neuralTraining(self):
+        send = neuralAnalisys()
 
         try:
 
-            neural = neuralAnalisys()
-            dataSet = neural.getValuesAPI()
-
             datecollect = [x[0] for x in dataSet]
             servers = [x[1] for x in dataSet]
-            historyoriginal = [float(x[2]) for x in dataSet]
-            #del historyoriginal[1]
+            valuesAnalisys = [float(x[2]) for x in dataSet]
 
-            base = np.array(historyoriginal)
+            base = np.array(valuesAnalisys)
 
-            periodos = len(historyoriginal) - 1
-            previsao_futura = 1 # horizonte
+            periods = len(valuesAnalisys) - 1
+            future_forecast = 1
 
-            X = base[0:(len(base) - (len(base) % periodos))]
-            X_batches = X.reshape(-1, periodos, 1)
+            X = base[0:(len(base) - (len(base) % periods))]
+            X_batches = X.reshape(-1, periods, 1)
 
-            y = base[1:(len(base) - (len(base) % periodos)) + previsao_futura]
-            y_batches = y.reshape(-1, periodos, 1)
+            y = base[1:(len(base) - (len(base) % periods)) + future_forecast]
+            y_batches = y.reshape(-1, periods, 1)
 
-            X_teste = base[-(periodos + previsao_futura):]
-            X_teste = X_teste[:periodos]
-            X_teste = X_teste.reshape(-1, periodos, 1)
-            y_teste = base[-(periodos):]
-            y_teste = y_teste.reshape(-1, periodos, 1)
+            X_test = base[-(periods + future_forecast):]
+            X_test = X_test[:periods]
+            X_test = X_test.reshape(-1, periods, 1)
+            y_test = base[-(periods):]
+            y_test = y_test.reshape(-1, periods, 1)
 
             tf.reset_default_graph()
 
-            entradas = 1
-            neuronios_oculta = 100
-            neuronios_saida = 1
+            appetizer = 1
+            hidden_neurons = 100
+            exit_neurons = 1
 
-            xph = tf.placeholder(tf.float32, [None, periodos, entradas])
-            yph = tf.placeholder(tf.float32, [None, periodos, neuronios_saida])
+            xph = tf.placeholder(tf.float32, [None, periods, appetizer])
+            yph = tf.placeholder(tf.float32, [None, periods, exit_neurons])
 
-            celula = tf.contrib.rnn.BasicRNNCell(num_units = neuronios_oculta, activation = tf.nn.relu)
-            # camada saida
-            celula = tf.contrib.rnn.OutputProjectionWrapper(celula, output_size = 1)
+            cell = tf.contrib.rnn.BasicRNNCell(num_units = hidden_neurons, activation = tf.nn.relu)
 
-            saida_rnn, _ = tf.nn.dynamic_rnn(celula, xph, dtype = tf.float32)
-            erro = tf.losses.mean_squared_error(labels = yph, predictions = saida_rnn)
+            cell = tf.contrib.rnn.OutputProjectionWrapper(cell, output_size = 1)
+
+            exit_rnn, _ = tf.nn.dynamic_rnn(cell, xph, dtype = tf.float32)
+            calculateError = tf.losses.mean_squared_error(labels = yph, predictions = exit_rnn)
             otimizador = tf.train.AdamOptimizer(learning_rate = 0.001)
-            treinamento = otimizador.minimize(erro)
+            training = otimizador.minimize(calculateError)
 
             with tf.Session() as sess:
                 sess.run(tf.global_variables_initializer())
                 
-                for epoca in range(200):
-                    _, custo = sess.run([treinamento, erro], feed_dict = {xph: X_batches, yph: y_batches})
-                    if epoca % 100 == 0:
-                        print(epoca + 1, ' Error: ', custo)
+                for epoch in range(1000):
+                    _, cost = sess.run([training, calculateError], feed_dict = {xph: X_batches, yph: y_batches})
+                    if epoch % 100 == 0:
+                        print("[INFO] Epoch: {} - Level Error: {}".format(epoch,cost))
                 
-                previsoes = sess.run(saida_rnn, feed_dict = {xph: X_teste})
-            y_teste.shape
-            y_teste2 = np.ravel(y_teste)
+                forecast = sess.run(exit_rnn, feed_dict = {xph: X_test})
 
-            previsoes2 = np.ravel(previsoes)
+            y_test.shape
+            y_test2 = np.ravel(y_test)
 
-            # level error
-            mae = mean_absolute_error(y_teste2, previsoes2)
+            final_forecast = np.ravel(forecast)
 
-            # Unindo valores originais com previstos e fazendo insert
+            mae = mean_absolute_error(y_test2, final_forecast)
+
             errorLevel = str(mae)
-            #dataModeling = map(lambda e: (dateTimeObj, e, errorLevel), previsoes2)
-            dataModeling = list(zip(previsoes2, historyoriginal, datecollect, servers))
 
-            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-            api_URL_SET = 'http://127.0.0.1:5000/setForecast'
+            dataModeling = list(zip(servers, final_forecast, valuesAnalisys, errorLevel, datecollect))
 
-            for prev, ori, collect, srv in dataModeling:
-                print("INSERT into forecastmemoryconsumption (date, forecastvalue, originalValue, datecollect, hostname, levelerror) VALUES (current_timestamp, '{}', '{}', '{}', '{}', '{}')".format(prev, ori, collect, srv, errorLevel))
-
-            # testing
-            try:
-
-                payload = {"idci": "12525", "forecastmemory": "90", "forecastcpu": "85",
-                            "forecastcapacity": "85","forecastuptime": "85","levelerror": "85",
-                            "datecollect": "2020-04-01","dateforecast": "2020-04-01"}
-
-                r = requests.post(api_URL_SET, auth=HTTPBasicAuth('root', 'root'), data=json.dumps(payload), headers=headers)
-                print(r.status_code)
-
-            except:
-
-                print("[ALERT] Error caused by credentials or request")
+            for servers, final_forecast, valuesAnalisys, errorLevel, datecollect in dataModeling:
+                send.postForecastMemory(servers, final_forecast, errorLevel, datecollect)
 
         except Exception as e:
             print("[ERROR] Error caused by: {}".format(e))
 
+    def postForecastMemory(self,hostname,memory,lvlerror,collected):
+        try:
+
+            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+            api_URL_SET = 'http://127.0.0.1:5000/setForecastMemory'
+
+            payload = {"server": "{}".format(hostname), 
+                        "forecastmemory": "{}".format(memory), 
+                        "levelerror": "{}".format(lvlerror), 
+                        "datecollect": collected}
+
+            r = requests.post(api_URL_SET, auth=HTTPBasicAuth('root', 'root'), data=json.dumps(payload), headers=headers)
+            
+            if(r.status_code == 200):
+                print("[INFO] Data insert status: {}".format(r.status_code))
+            else:
+                print("[INFO] Data insert status: {}".format(r.status_code))
+
+        except Exception as e:
+
+            print("[ALERT] Error caused by {}".format(e))
+
 if __name__ == "__main__":
     
-    flow = neuralAnalisys()
-    flow.neuralTraining()
+    flow = initTraining()
+    flow.startProcessing()
