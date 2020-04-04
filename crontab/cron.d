ca8b1da2ad0f5d82a698d2b7e4e202ede8b8980c
vinhali <43426743+vinhali@users.noreply.github.com>
2020-04-03 22:14:24 -0300

    Update cron.d

--- crontab/cron.d
+++ crontab/cron.d
@@ -4,7 +4,7 @@
 #Collect CPU in API Zabbix
 * * 1 * * /usr/bin/python3.7 /etc/neural/getValues.py "http://192.168.1.135/zabbix" "Admin" "zabbix" "Servers Production" "Uso do processador na média de 1 minuto" 90 "127.0.0.1" "networkneural" "postgres" "postgres" "CPUEXPORTZB" >> /etc/neural/logs/collectCpu.log
 
-# Collect Disk in API
+#Collect Disk in API
 * * 1 * * /usr/bin/python3.7 /etc/neural/getValues.py "http://192.168.1.135/zabbix" "Admin" "zabbix" "Servers Production" "Disco total Free (%)" 90 "127.0.0.1" "networkneural" "postgres" "postgres" "DISKEXPORTZB" >> /etc/neural/logs/collectDisk.log
 
 #Analisys neural for Memory
