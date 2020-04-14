9dbf15f40b3f46b1df6ee6a383485374eab24bb4
vinhali <43426743+vinhali@users.noreply.github.com>
2020-04-13 22:24:42 -0300

    Create poly.py

---
+++ polynomial_regression/multiple-date/poly.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# Importing the dataset
+dataset = pd.read_csv('data.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Visualizing the Linear Regression results
+def viz_linear():
+    plt.scatter(X, y, color='red')
+    plt.plot(X, lin_reg.predict(X), color='blue')
+    plt.title('Linear Regression for CPU')
+    plt.xlabel('Time range')
+    plt.ylabel('Consume')
+    plt.show()
+    return
+#viz_linear()
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree=4)
+X_poly = poly_reg.fit_transform(X)
+pol_reg = LinearRegression()
+pol_reg.fit(X_poly, y)
+
+# Visualizing the Polymonial Regression results
+def viz_polymonial():
+    plt.scatter(X, y, color='red')
+    indices = np.argsort(X[:, 0])
+    plt.scatter(X, pol_reg.predict(poly_reg.fit_transform(X)), color='green')
+    plt.plot(X[indices], pol_reg.predict(poly_reg.fit_transform(X))[indices], color='black')
+    plt.title('Polynomial Regression for CPU')
+    plt.xlabel('Time range')
+    plt.ylabel('Consume')
+    plt.show()
+    return
+viz_polymonial()
+
+# 20 = time
+print(pol_reg.predict(poly_reg.fit_transform([[20]])))
